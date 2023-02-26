63fc789f3d585a68981616fa19082ee7a83b379e
Hyeondoonge <migu554@naver.com>
2023-02-27 00:58:00 +0900

    :sparkles: db 관련 error 코드 및 메세지 추가 - #6

--- errorMessage.ts
+++ errorMessage.ts
@@ -4,6 +4,8 @@ const ErrorMessage: Record<ErrorCode, string> = {
   U001: 'access with no authentication',
   U002: 'access with no authorization',
   U003: 'try to access to nonexistent content',
+  D001: 'db create error',
+  D002: 'db connection error',
 }
 
 export default ErrorMessage
